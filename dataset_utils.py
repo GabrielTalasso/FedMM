ff8f16cbd2e4ed48e324fbde9a932d0c9b6278fb
GabrielTalasso <gukstin@gmail.com>
2024-02-20 17:17:39 -0300

    modified:   dataset_utils.py 	deleted:    run.py

--- dataset_utils.py
+++ dataset_utils.py
@@ -3,6 +3,7 @@ import numpy as np
 import random
 import pickle
 import pandas as pd
+from scipy.stats import dirichlet, multinomial
 #from sklearn.preprocessing import Normalizer
 
 import logging
@@ -10,9 +11,55 @@ logging.getLogger("tensorflow").setLevel(logging.ERROR)
 
 class ManageDatasets():
 
-	def __init__(self, cid):
+	def __init__(self, cid, dataset_name):
 		self.cid = cid
-		#random.seed(self.cid)
+		self.dataset_name = dataset_name
+
+	def load_from_keras(self,
+		dataset_size, #for each client
+		alpha, #param of dirichlet distribuition
+		test_size = 0.2): # proportion of dataset to use as test set
+
+
+		dataset = self.get_dataset_from_keras(self.dataset_name)      
+		(x_train, y_train), (x_test, y_test) = dataset.load_data()
+		n_classes = len(np.unique(y_train)) #number of classes in dataset
+
+		alpha_vector = alpha * np.ones(n_classes)
+
+		client_proportions = dirichlet.rvs(alpha_vector, size = 1)
+		client_quantities = []
+
+		client_quantities = multinomial.rvs(n = dataset_size, p = client_proportions[0]) #quantity of each class
+
+		index_train = []
+		index_test = []
+		for i, n in enumerate(client_quantities): #pass for all classes
+
+			try:
+				index_train = np.append(index_train, 
+									np.random.choice(np.where(y_train == i)[0], int(n * (1 - test_size)))) #choose the exact quantity of each class, randomly
+			except ValueError: #the client may not have a label
+				pass
+
+			try:
+				index_test = np.append(index_test, 
+									np.random.choice(np.where(y_test == i)[0], int(n * test_size)))
+			except ValueError:
+				pass
+		
+		index_train = index_train.astype(int)
+		index_test = index_test.astype(int)
+
+		x_train = x_train[index_train]
+		y_train = y_train[index_train]
+
+		x_test = x_test[index_test]
+		y_test = y_test[index_test]
+
+
+		return (x_train, y_train), (x_test, y_test)
+
 
 	def load_UCIHAR(self):
 		with open(f'data/UCI-HAR/{self.cid +1}_train.pickle', 'rb') as train_file:
@@ -73,160 +120,41 @@ class ManageDatasets():
 		x_test = test.values
 	    
 		return x_train, y_train, x_test, y_test
+	
+	def get_dataset_from_keras(self):
+		#Loads a dataset from Keras based on its name and returns the class of dataset
 
+		if self.dataset_name == 'MNIST':
+			return tf.keras.datasets.mnist
 
-	def load_MNIST(self, n_clients, non_iid=False, Xnon_iid = False):
+		elif self.dataset_name == 'CIFAR10':
+			return tf.keras.datasets.cifar10
 
+		elif self.dataset_name == 'CIFAR100':
+			return tf.keras.datasets.cifar100
 
-		if non_iid:
+		elif self.dataset_name == 'FMNIST':
+			return tf.keras.datasets.fashion_mnist
 
-			if Xnon_iid:
+		elif self.dataset_name == 'IMDB':
+			return tf.keras.datasets.imdb
 
-				with open(f'data/MNIST/Xnon_iid/{n_clients}/idx_train_{self.cid}.pickle', 'rb') as handle:
-					idx_train = pickle.load(handle)
-					#idx_train = np.random.choice(idx_train, int(len(idx_train)*0.01))
+		elif self.dataset_name == 'REUTERS':
+			return tf.keras.datasets.reuters
 
-				with open(f'data/MNIST/Xnon_iid/{n_clients}/idx_test_{self.cid}.pickle', 'rb') as handle:
-					idx_test = pickle.load(handle)
-					#idx_test = np.random.choice(idx_test, int(len(idx_test)*0.01))
+	def select_dataset(self,
+					dataset_size = None,
+					alpha = None, 
+					test_size = 0.2):
 
-				#idx_train = idx_train[int(self.cid)]
-				#idx_test = idx_test[int(self.cid)]
+		if self.dataset_name == 'MotionSense':
+			return self.load_MotionSense()	
 			
-				(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
-				x_train, x_test                      = x_train/255.0, x_test/255.0
-
-				x = np.concatenate([x_train, x_test], axis=0)
-				y = np.concatenate([y_train, y_test], axis=0)
-
-				x_train = x[idx_train]
-				x_test  = x[idx_test]
-
-				y_train = y[idx_train]
-				y_test  = y[idx_test]
-
-			else:
-
-				with open(f'data/MNIST/{n_clients}/idx_train_{self.cid}.pickle', 'rb') as handle:
-					idx_train = pickle.load(handle)
-
-				with open(f'data/MNIST/{n_clients}/idx_test_{self.cid}.pickle', 'rb') as handle:
-					idx_test = pickle.load(handle)
-
-
-				(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
-				x_train, x_test                      = x_train/255.0, x_test/255.0
-
-				x_train = x_train[idx_train] 
-				x_test  = x_test[idx_test]
-
-				y_train = y_train[idx_train]
-				y_test  = y_test[idx_test]
-			
-
-		else:
-
-			(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
-			x_train, x_test                      = x_train/255.0, x_test/255.0
-			x_train, y_train, x_test, y_test     = self.slipt_dataset(x_train, y_train, x_test, y_test, n_clients)
-
-		return x_train, y_train, x_test, y_test
-
-	def load_CIFAR10(self, n_clients, non_iid=False, Xnon_iid = False):
-		
-		if non_iid:
-
-			if Xnon_iid:
-
-				with open(f'data/{n_clients}/idx_train_{self.cid}.pickle', 'rb') as handle:
-					idx_train = pickle.load(handle)
-
-				with open(f'data/{n_clients}/idx_test_{self.cid}.pickle', 'rb') as handle:
-					idx_test = pickle.load(handle)
-			
-				(x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
-				x_train, x_test                      = x_train/255.0, x_test/255.0
-
-				x = np.concatenate([x_train, x_test], axis=0)
-				y = np.concatenate([y_train, y_test], axis=0)
-
-				x_train = x[idx_train]
-				x_test  = x[idx_test]
-
-				y_train = y[idx_train]
-				y_test  = y[idx_test]
-
-			else:
-			
-				with open(f'data/CIFAR10/{n_clients}/idx_train_{self.cid}.pickle', 'rb') as handle:
-					idx_train = pickle.load(handle)
-
-				with open(f'data/CIFAR10/{n_clients}/idx_test_{self.cid}.pickle', 'rb') as handle:
-					idx_test = pickle.load(handle)
-
-
-				(x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
-				x_train, x_test                      = x_train/255.0, x_test/255.0
-
-				x_train = x_train[idx_train]
-				x_test  = x_test[idx_test]
-
-				y_train = y_train[idx_train]
-				y_test  = y_test[idx_test]
-				
-		else:
-
-			(x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
-			x_train, x_test                      = x_train/255.0, x_test/255.0
-			x_train, y_train, x_test, y_test     = self.slipt_dataset(x_train, y_train, x_test, y_test, n_clients)
-		
-		return x_train, y_train, x_test, y_test
-
-	def slipt_dataset(self, x_train, y_train, x_test, y_test, n_clients):
-		p_train = int(len(x_train)/(10*n_clients))
-		p_test  = int(len(x_test)/(10*n_clients))
-
-
-		random.seed(self.cid)
-		selected_train = random.sample(range(len(x_train)), p_train)
-
-		random.seed(self.cid)
-		selected_test  = random.sample(range(len(x_test)), p_test)
-		
-		x_train  = x_train[selected_train]
-		y_train  = y_train[selected_train]
-
-		x_test   = x_test[selected_test]
-		y_test   = y_test[selected_test]
-
-
-		return x_train, y_train, x_test, y_test
-
-
-	def select_dataset(self, dataset_name, n_clients, non_iid, Xnon_iid = False):
-
-		if dataset_name == 'MNIST':
-			return self.load_MNIST(n_clients, non_iid, Xnon_iid)
-
-		elif dataset_name == 'CIFAR100':
-			return self.load_CIFAR100(n_clients, non_iid)
-
-		elif dataset_name == 'CIFAR10':
-			return self.load_CIFAR10(n_clients, non_iid, Xnon_iid)
-
-		elif dataset_name == 'MotionSense':
-			return self.load_MotionSense()
-		
-		elif dataset_name == 'ExtraSensory':
+		elif self.dataset_name == 'ExtraSensory':
 			return self.load_ExtraSensory()
-
-		elif dataset_name == 'UCIHAR':
+		
+		elif self.dataset_name == 'UCIHAR':
 			return self.load_UCIHAR()
-
-
-	def normalize_data(self, x_train, x_test):
-		x_train = Normalizer().fit_transform(np.array(x_train))
-		x_test  = Normalizer().fit_transform(np.array(x_test))
-		return x_train, x_test
-
-
+			
+		elif self.dataset_name in ['MNIST','CIFAR10', 'CIFAR100', 'FMNIST', 'IMDB', 'REUTERS']:
+			return self.load_from_keras(dataset_size = dataset_size, alpha = alpha, test_size=test_size)
