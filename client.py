41d540153f4de974a5c00bfdea67f64acbb1b863
GabrielTalasso <gukstin@gmail.com>
2024-02-21 08:02:31 -0300

    new data for HAR

--- client.py
+++ client.py
@@ -20,18 +20,24 @@ from sys import getsizeof
 
 class ClientBase(fl.client.NumPyClient):
 
-	def __init__(self, cid, dataset, n_clients, model_name, local_epochs, non_iid, Xnon_iid, 
+	def __init__(self, cid, dataset, n_clients, model_name, local_epochs,
 	      n_rounds, n_clusters, selection_method, cluster_metric, 
 		  cluster_method, metric_layer = -1, 
-		  POC_perc_of_clients = 0.5):
+		  POC_perc_of_clients = 0.5,
+		  dir_alpha = 100,
+		  dataset_size = 1000,
+		  dataset_n_classes = 10):
 
 		self.cid = cid
 		self.round = 0
 		self.n_clients = n_clients
+
 		self.dataset = dataset
+		self.dir_alpha = dir_alpha #dirichlet alpha
+		self.dataset_size = dataset_size
+		self.dataset_n_classes = dataset_n_classes
+
 		self.model_name = model_name
-		self.non_iid = non_iid
-		self.Xnon_iid = Xnon_iid
 		self.local_epochs = local_epochs
 
 		self.selection_method = selection_method
@@ -47,16 +53,18 @@ class ClientBase(fl.client.NumPyClient):
 		self.n_clusters = n_clusters
 
 	def load_data(self):
-		return ManageDatasets(self.cid).select_dataset(self.dataset, self.n_clients, self.non_iid, self.Xnon_iid)
+		return ManageDatasets(self.cid, dataset_name=self.dataset).select_dataset(self.n_clients,
+																				  alpha = self.dir_alpha,
+																				  dataset_size = self.dataset_size)
 
 	def create_model(self):
 		input_shape = self.x_train.shape
 
 		if self.model_name == 'DNN':
-			return ModelCreation().create_DNN(input_shape, 8)
+			return ModelCreation().create_DNN(input_shape, self.dataset_n_classes)
 
 		elif self.model_name == 'CNN':
-			return ModelCreation().create_CNN(input_shape, 8)
+			return ModelCreation().create_CNN(input_shape, self.dataset_n_classes)
 		
 
 	def get_parameters(self, config):
@@ -64,39 +72,21 @@ class ClientBase(fl.client.NumPyClient):
 
 	def fit(self, parameters, config):
 
-		#config recebe parametros do servidor
 		self.model.set_weights(parameters)
 		h = self.model.fit(self.x_train, self.y_train, 
 		                    validation_data = (self.x_test, self.y_test),
 							verbose=1, epochs=self.local_epochs)
 
-		# '''
-		# adicionar metodos
-		# metricas
-		# ativações
-		# modelos
-		# '''
-
 		msg2server = {
 				"cliente_id": self.cid,
-				# "ativacoes" : var_ativacoes
 		}
 		self.round += 1
 	
-		#with open(f'results/acc_train_{self.dataset}_{self.n_clients}clients_{self.n_clusters}clusters.csv', 'a') as arquivo:
-		#	arquivo.write(f"{config['round']}, {self.cid}, {np.mean(h.history['accuracy'])}, {np.mean(h.history['loss'])}\n")
-	 		
 		filename = f"local_logs/{self.dataset}/{self.cluster_metric}-({self.metric_layer})-{self.cluster_method}-{self.selection_method}-{self.POC_perc_of_clients}/train/acc_{self.n_clients}clients_{self.n_clusters}clusters.csv"
 		os.makedirs(os.path.dirname(filename), exist_ok=True)
 		with open(filename, 'a') as arquivo:
 			arquivo.write(f"{config['round']}, {self.cid}, {np.mean(h.history['accuracy'])}, {np.mean(h.history['loss'])}\n")
 
-		#acc = pd.read_csv('/content/acc.csv')		
-		#acc = acc.append({'cid':self.cid, 
-		#            'acc':accuracy}, ignore_index = True)
-		#acc[['cid', 'acc']].to_csv('acc.csv')
-
-		#config usado para passarar parametros para o servidor
 		return self.model.get_weights(), len(self.x_train), msg2server
 
 
